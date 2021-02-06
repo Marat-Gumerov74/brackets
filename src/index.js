1e6fee0752eebfd1727702e41fc9201f32aa281d
Marat Gumerov <gumerovm74@gmail.com>
2021-02-06 13:07:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let map = new Map(bracketsConfig);
+  let arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+      (str[i] === arr[arr.length - 1]) ? arr.pop(): arr.push(map.get(str[i]));
+  }
+  return arr.length === 0;
 }
